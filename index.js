688a1690baf900a505cacd78eba00477c637b305
Qayyum Suhaimi <qayyumsuhaimi10@gmail.com>
2024-07-20 02:44:23 +0800

    Add

--- index.js
+++ index.js
@@ -19,12 +19,17 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res){
+  const ipaddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  const language = req.headers['accept-language'];
+
+  const software = req.headers['user-agent'];
+
+  res.json({ ipaddress, language, software });
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 4000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
