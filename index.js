ce0f2c39d4516cc404efdf77c95d5363ab305794
Deepanshu3130 <djjoshi3130@gmail.com>
2024-08-12 14:28:25 +0530

    given task done

--- index.js
+++ index.js
@@ -20,8 +20,17 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+
+
+  const ipaddress = req.socket.remoteAddress;
+  const language = req.headers['accept-language'] ? req.headers['accept-language'].split(',')[0]: "undefined"
+  const software = req.headers['user-agent'] || 'unknown';
+  res.json({ 
+    ipaddress:ipaddress,
+    language:language,
+    software: software
+   });
 });
 
 // listen for requests :)
