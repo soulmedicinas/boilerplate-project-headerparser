ed47c5ca199078b7de3c4e1a52a49172b6eee5a7
Andi Muhammad Riffal <andimuhriffal21@gmail.com>
2024-12-01 12:57:15 +0000

    MyBootCamp

--- index.js
+++ index.js
@@ -24,7 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res) {
+  var language = req.acceptsLanguages();
+
+  res.json({ 
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log('Your app is listening on port ' + 
+    listener.address().port);
 });
