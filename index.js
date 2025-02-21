e89255c2d19ba6b8f20b377fdcdfccced4874c66
Abhay Jain <abhayforwork02@gmail.com>
2025-02-21 18:39:47 +0000

    done

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", function(req, res) {
+  const ip = req.ip;
+  const lan = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+  res.json({
+    ipaddress: ip,
+    language: lan,
+    software: software,
+  })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
