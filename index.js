8c0182bafe789c80fe742743a26edfa80a26b091
tira_menali <77180304+soulmedicinas@users.noreply.github.com>
2025-04-02 11:53:50 -0700

    add: set trust proxy

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
+app.set('trust proxy', true); // Enable for accurate IP behind proxies
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
