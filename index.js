0c800b585f37d6916e785cdbd284df00d5b07ebd
tira_menali <77180304+soulmedicinas@users.noreply.github.com>
2025-04-02 11:01:51 -0700

    add: header parser endpoint

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// Header parser endpoint
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.ip, // Extracts IP (handles proxies via Express trust proxy)
+    language: req.headers['accept-language'], // Gets language preference
+    software: req.headers['user-agent'] // Gets browser/OS info
+  });
+});
+
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
